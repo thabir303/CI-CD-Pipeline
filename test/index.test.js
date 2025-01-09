789a3123935d8391e2039ae160ceea46dea3d991
th_abir303 <bsse1321@iit.du.ac.bd>
2025-01-09 07:40:03 +0600

    test case will fail

--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,6 @@ const { add } = require('../index');
 
 test('adds two numbers correctly', () => {
   expect(add(1, 2)).toBe(3);
-  expect(add(1, "asdf")).toBe("Invalid Input");
+  // expect(add(1, "asdf")).toBe("Invalid Input");
   expect(add(1, "acdef")).toBe(4);
 });
