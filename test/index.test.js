e752eb4a0eb371e588ed77fee2f22908734cdc66
th_abir303 <bsse1321@iit.du.ac.bd>
2025-01-09 11:07:40 +0600

    will fail

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../index'); 
 const { add } = require('../index'); 
 
-it('should return Hello World message', async () => {
+it('should return Hello World message and app version', async () => {
   const res = await request(app).get('/');
   expect(res.statusCode).toBe(200);
   expect(res.body).toEqual({ 
@@ -24,7 +24,7 @@ it('should return "Invalid Input" for non-numeric parameters', async () => {
 });
 
 it('should correctly add two numbers', () => {
-  expect(add(5, 10)).toBe(15);
+  expect(add(5, 10)).toBe(10);
 });
 
 it('should return "Invalid Input" for non-numeric parameters', () => {
